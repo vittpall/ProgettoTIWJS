61af381f601f1d2dca512a0e172fd729f945a26a
vittpall <vittorio.palladino@mail.polimi.it>
2024-05-03 01:01:22 +0200

    add registration

---
+++ src/main/webapp/registration.js
@@ -0,0 +1,37 @@
+/**
+ * Registration management
+ */
+
+(function() { // avoid variables ending up in the global scope
+
+  document.getElementById("loginRegistration").addEventListener('click', (e) => {
+    var form = e.target.closest("form");
+    if (form.checkValidity()) {
+      makeCall("POST", 'CheckLogin', e.target.closest("form"),
+        function(x) {
+          if (x.readyState == XMLHttpRequest.DONE) {
+            var message = x.responseText;
+            switch (x.status) {
+              case 200:
+            	sessionStorage.setItem('username', message);
+                window.location.href = "Home.html";
+                break;
+              case 400: // bad request
+                document.getElementById("loginError").textContent = message;
+                break;
+              case 401: // unauthorized
+                  document.getElementById("loginError").textContent = message;
+                  break;
+              case 500: // server error
+            	document.getElementById("loginError").textContent = message;
+                break;
+            }
+          }
+        }
+      );
+    } else {
+    	 form.reportValidity();
+    }
+  });
+
+})();
